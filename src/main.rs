8d31f8c910c426cb146c833dd89f75abd2f7c076
Mroik <mroik@delayed.space>
2025-01-28 00:37:04 +0100

    Change quote option specifier

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use rand::{thread_rng, Rng};
 #[derive(Parser)]
 struct Args {
     #[arg(short, long)]
-    quote_folder: String,
+    quote: String,
 }
 
 fn generate_quotes(path: &Path) -> Result<Vec<String>, Box<dyn Error>> {
@@ -35,7 +35,7 @@ fn generate_quotes(path: &Path) -> Result<Vec<String>, Box<dyn Error>> {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    let path = Path::new(&args.quote_folder);
+    let path = Path::new(&args.quote);
     let mut quotes = generate_quotes(&path).unwrap();
     let mut rng = thread_rng();
     let chosen = rng.gen_range(0..quotes.len());
